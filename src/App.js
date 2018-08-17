5bb53cc2ae4fefd210c79bd7677d85a11997743d
unknown <maahboss@yahoo.com>
2018-08-17 16:18:33 +0300

    First init PErson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
+
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> It is Moh , Hey </h1>
+        <p>Hello Another paraghraph</p>
+        <Person />
+        
       </div>
+      
 
       
     );
